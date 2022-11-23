3160980f2527dc2bef22e3be55fcad3d42b9c1cb
Athaliah <athaliahtauro@gmail.com>
2022-11-23 21:37:33 +0200

    Add full navigation overlay and contact validatation

--- script.js
+++ script.js
@@ -1,5 +1,58 @@
-function setUp() {
+const menuButtonOpen = document.getElementById('mobile-menu');
+const menuButtonClose = document.getElementById('mobile-menu-exit');
+const navListMenu = document.getElementById('nav-list-menu');
+const navItems = document.querySelectorAll('.nav-item');
 
-    document.getElementsByClassName ("menu").onclick =setMenu;
-    
+function menuToggle() {
+  if (navListMenu.classList.contains('open')) {
+    navListMenu.classList.remove('open');
+    menuButtonOpen.style.display = 'block';
+    menuButtonClose.style.display = 'none';
+    navListMenu.style.display = 'none';
+  } else {
+    navListMenu.classList.add('open');
+    menuButtonOpen.style.display = 'none';
+    menuButtonClose.style.display = 'block';
+    navListMenu.style.display = 'block';
+  }
 }
+
+menuButtonOpen.addEventListener('click', menuToggle);
+menuButtonClose.addEventListener('click', menuToggle);
+navItems.forEach((navItem) => {
+  navItem.addEventListener('click', menuToggle);
+});
+function outputMessage(accepted) {
+  const message = document.getElementById('msg');
+  if (accepted) {
+    message.classList.remove('error');
+  } else {
+    message.classList.add('error');
+    message.innerHTML = 'Please enter a correct email address format';
+  }
+}
+
+const form = document.querySelector('.container-form');
+
+form.addEventListener('submit', (element) => {
+  const email = document.getElementById('email').value;
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (email !== email.toLowerCase()) {
+    element.preventDefault();
+    outputMessage(false);
+    return;
+  }
+  if (!emailRegex.test(email)) {
+    element.preventDefault();
+    outputMessage(false);
+    return;
+  }
+  outputMessage(true);
+});
+
+
+
+
+
+
