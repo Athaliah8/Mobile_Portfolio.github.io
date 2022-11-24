22ff7c00914b9039b795f2259f1920a5cca069c1
Athaliah <athaliahtauro@gmail.com>
2022-11-24 10:10:52 +0200

    update

---
+++ js/pop.js
@@ -0,0 +1,11 @@
+const open = document.querySelector('.see-btn');
+open.addEventListener ('click', ()=>
+
+
+{
+    const main = document.createElement('div');
+    main.className ='main';
+    const popup = document.createElement('div');
+    popup.innerHTML = main.appendChild(popup)
+    
+})
