af515c6bb8ac8b3777567ba3895eb5d912f4d77b
Athaliah <athaliahtauro@gmail.com>
2022-11-25 10:50:02 +0200

    Update changes to linter files

---
+++ js/index.js
@@ -0,0 +1,60 @@
+import data from './info.js';
+
+const cardList = document.querySelector('.cards-list');
+
+// POPUP WINDOW //
+
+const cardPopupWindow = document.querySelector('.card-popup-window');
+const seeProjectBtn = document.querySelectorAll('.see-btn');
+const seeProjectImg = document.querySelectorAll('.card-image');
+const closedWind = document.querySelector('#close-pop');
+
+const arr = [seeProjectBtn, seeProjectImg];
+arr.forEach((item) => {
+  item.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      cardPopupWindow.classList.add('open-pop-up');
+      const filteredCard = data.filter((card) => card.id === Number(link.dataset.indexNumber));
+      const title = document.querySelector('.card-popup-window .popup-main-container .main-container .title h2');
+      const info = document.querySelectorAll('.card-popup-window .main-container .info span');
+      const img = document.querySelector('.card-popup-window .main-container .img-container img');
+      const detail = document.querySelector('.card-popup-window .main-container .text-container p');
+      const techno = document.querySelector('#lang');
+      const links = document.querySelectorAll('.card-popup-window .main-container .about-links a');
+
+      title.innerHTML = filteredCard[0].name;
+      img.src = filteredCard[0].image;
+      detail.innerHTML = filteredCard[0].fullDesciption;
+
+      for (let i = 0; i < Array.from(info).length; i += 1) {
+        if (i === 0) {
+          info[i].innerHTML = filteredCard[0].description.type;
+        } else if (i === 2) {
+          info[i].innerHTML = filteredCard[0].description.interface;
+        } else if (i === 4) {
+          info[i].innerHTML = filteredCard[0].description.year;
+        }
+      }
+
+      const technologiesChild = [...techno.childNodes];
+      technologiesChild.forEach((child, index) => {
+        techno.removeChild(technologiesChild[index]);
+      });
+      filteredCard[0].technologies.forEach((item) => {
+        const techItem = document.createElement('li');
+        const techLink = document.createElement('a');
+        techLink.href = '#';
+        techLink.innerText = item;
+        techItem.appendChild(techLink);
+        techno.appendChild(techItem);
+      });
+      links[0].href = filteredCard[0].liveDemo;
+      links[1].href = filteredCard[0].sourceCode;
+    });
+  });
+});
+
+closedWind.addEventListener('click', () => {
+  cardPopupWindow.classList.remove('open-pop-up');
+});
--- js/info.js
+++ js/info.js
@@ -1,72 +1,72 @@
 const data = [
-    {
-      id: 1,
-      image: './images/eerste.svg',
-      name: 'Tonic',
-      description: {
-        type: 'CANOPY',
-        interface: 'Backend Dev',
-        year: 2015,
-      },
-      fullDesciption:
+  {
+    id: 1,
+    image: './images/eerste.svg',
+    name: 'Tonic',
+    description: {
+      type: 'CANOPY',
+      interface: 'Backend Dev',
+      year: 2015,
+    },
+    fullDesciption:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eius quae optio et quas, quibusdam quis excepturi omnis? Quos, officia accusamus. Aperiam, molestiae elit. atque maiores aperiam.    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita ex quam officia ea dolor. Ea quasi in id dolorum ',
-      detail:
+    detail:
         'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-      technologies: ['Html', 'Css', 'Javascript', 'Ruby on Rails'],
-      liveDemo: 'https://github.com/Athaliah8',
-      sourceCode: 'https://github.com/Athaliah8',
+    technologies: ['Html', 'Css', 'Javascript', 'Ruby on Rails'],
+    liveDemo: 'https://github.com/Athaliah8',
+    sourceCode: 'https://github.com/Athaliah8',
+  },
+  {
+    id: 2,
+    image: './images/eerste.svg',
+    name: 'Multi-Post Stories',
+    description: {
+      type: 'CANOPY',
+      interface: 'Backend Dev',
+      year: 2015,
     },
-    {
-      id: 2,
-      image: './images/eerste.svg',
-      name: 'Multi-Post Stories',
-      description: {
-        type: 'CANOPY',
-        interface: 'Backend Dev',
-        year: 2015,
-      },
-      fullDesciption:
+    fullDesciption:
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-      detail:
+    detail:
         'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-      technologies: ['Html', 'Css', 'Javascript', 'Ruby on Rails'],
-      liveDemo: 'https://athaliah8.github.io/Mobile_Portfolio/',
-      sourceCode: 'https://github.com/Athaliah8',
+    technologies: ['Html', 'Css', 'Javascript', 'Ruby on Rails'],
+    liveDemo: 'https://athaliah8.github.io/Mobile_Portfolio/',
+    sourceCode: 'https://github.com/Athaliah8',
+  },
+  {
+    id: 3,
+    image: './images/eerste.svg',
+    name: 'Tonic',
+    description: {
+      type: 'CANOPY',
+      interface: 'Backend Dev',
+      year: 2015,
     },
-    {
-      id: 3,
-      image: './images/eerste.svg',
-      name: 'Tonic',
-      description: {
-        type: 'CANOPY',
-        interface: 'Backend Dev',
-        year: 2015,
-      },
-      fullDesciption:
+    fullDesciption:
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-      detail:
+    detail:
         'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-      technologies: ['Html', 'Css', 'Javascript', 'Ruby on Rails' ],
-      liveDemo: 'https://athaliah8.github.io/Mobile_Portfolio/',
-      sourceCode: 'https://github.com/Athaliah8',
+    technologies: ['Html', 'Css', 'Javascript', 'Ruby on Rails'],
+    liveDemo: 'https://athaliah8.github.io/Mobile_Portfolio/',
+    sourceCode: 'https://github.com/Athaliah8',
+  },
+  {
+    id: 4,
+    image: './images/eerste.svg',
+    name: 'Multi-Post Stories',
+    description: {
+      type: 'CANOPY',
+      interface: 'Backend Dev',
+      year: 2015,
     },
-    {
-      id: 4,
-      image: './images/eerste.svg',
-      name: 'Multi-Post Stories',
-      description: {
-        type: 'CANOPY',
-        interface: 'Backend Dev',
-        year: 2015,
-      },
-      fullDesciption:
+    fullDesciption:
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-      detail:
+    detail:
         'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-      technologies: ['Html', 'Css', 'Javascript', 'Ruby on Rails'],
-      liveDemo: 'https://athaliah8.github.io/Mobile_Portfolio/',
-      sourceCode: 'https://github.com/Athaliah8',
-    },
-   
-  ];
-  export default data;
+    technologies: ['Html', 'Css', 'Javascript', 'Ruby on Rails'],
+    liveDemo: 'https://athaliah8.github.io/Mobile_Portfolio/',
+    sourceCode: 'https://github.com/Athaliah8',
+  },
+
+];
+export default data;
